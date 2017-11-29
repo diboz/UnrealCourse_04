1803f90354bd0bae3ef1184cf7aebce6aeb6a257
BOZDESKTOP\Boz <jimbozikas@gmail.com>
2017-11-30 00:45:05 +0200

    C12 Player Controller Code Start

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -3,7 +3,19 @@
 #include "TankPlayerController.h"
 
 
+void ATankPlayerController::BeginPlay() {
 
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank) {
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possesing a tank!"));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController possesing %s"), *(ControlledTank->GetName()));
+	}
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"));
+}
 
 ATank* ATankPlayerController::GetControlledTank() const {
 
